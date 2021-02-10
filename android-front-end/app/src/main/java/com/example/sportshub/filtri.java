e10b4d081d4a742507d652550e4936e19a9c2989
DavideBaiosto <d.baiosto@itsrizzoli.it>
2021-02-10 16:01:04 +0100

    fitri.xml

---
+++ android-front-end/app/src/main/java/com/example/sportshub/filtri.java
@@ -0,0 +1,18 @@
+package com.example.sportshub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class filtri extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onStart();
+
+    }
+}
