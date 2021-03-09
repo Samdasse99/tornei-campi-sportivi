8170dde8c8056dd974fd06dab586ff1a78142695
LorenzoChiesa <76174240+LorenzoChiesa@users.noreply.github.com>
2021-03-09 15:22:24 +0100

    navbar component

--- angular-sportshub/sportshub/src/app/app-routing.module.ts
+++ angular-sportshub/sportshub/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
 import { DatiUtenteComponent } from './dati-utente/dati-utente.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registrazione', component: RegistrazioneComponent},
   {path: 'dati-utente', component: DatiUtenteComponent},
--- angular-sportshub/sportshub/src/app/app.module.ts
+++ angular-sportshub/sportshub/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
 import { DatiUtenteComponent } from './dati-utente/dati-utente.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrazioneComponent,
-    DatiUtenteComponent
+    DatiUtenteComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
