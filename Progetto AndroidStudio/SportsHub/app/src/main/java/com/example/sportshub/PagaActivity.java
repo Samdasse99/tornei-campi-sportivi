93337fb9496ef5544dae14067441f1dab6d512ba
DanieleBaratella <d.baratella@itsrizzoli.com>
2021-03-11 11:56:41 +0100

    .

--- Progetto AndroidStudio/SportsHub/app/src/main/java/com/example/sportshub/PagaActivity.java
+++ Progetto AndroidStudio/SportsHub/app/src/main/java/com/example/sportshub/PagaActivity.java
@@ -1,4 +1,14 @@
 package com.example.sportshub;
 
-public class PagaActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paga);
+    }
 }
--- Progetto AndroidStudio/SportsHub/app/src/main/java/com/example/sportshub/StrutturaActivity.java
+++ Progetto AndroidStudio/SportsHub/app/src/main/java/com/example/sportshub/StrutturaActivity.java
@@ -1,5 +1,6 @@
 package com.example.sportshub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -7,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 
 public class StrutturaActivity extends AppCompatActivity {
@@ -18,18 +21,24 @@ public class StrutturaActivity extends AppCompatActivity {
 
         Button buttonPaga = (Button) findViewById(R.id.buttonPaga);
         buttonPaga.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 openPagamenti();
             }
         });
     }
-public void  openPagamenti() {
-    Intent intent = new Intent(this,PagaActivity.class);
-    startActivity(intent);
+
+    public void openPagamenti() {
+        Intent intent = new Intent(this, PagaActivity.class);
+        startActivity(intent);
 
     }
+
 }
+
+
+
 
 
 
